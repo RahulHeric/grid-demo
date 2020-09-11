8c9295bc2c66139021822d8e4657c6ea1a0d2613
rahul.jesuraj <rahul.jesuraj@host.docker.internal>
2020-09-11 13:15:09 +0530

    3rd commit

--- src/test/java/docker/ChromeTest.java
+++ src/test/java/docker/ChromeTest.java
@@ -3,6 +3,7 @@ package docker;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
@@ -16,9 +17,12 @@ DesiredCapabilities dc=DesiredCapabilities.chrome();
 		URL url=new URL("http://localhost:4545/wd/hub");
 		
 		RemoteWebDriver driver=new RemoteWebDriver(url,dc);
-		Thread.sleep(5000);
+		String drive = driver.toString();
+		System.out.println(drive);
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 		Thread.sleep(5000);
+		String text = driver.findElement(By.id("logInPanelHeading")).getText();
+		System.out.println(text);
 		System.out.println("title of page : "+driver.getTitle());
 		driver.quit();
 		
